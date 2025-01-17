ec9519f12b2720e7c3b01572f00173d87210f23c
sydmasina <masinasydwell@gmail.com>
2025-01-17 07:55:34 +0200

    Add project files.

---
+++ DiaryApp/Models/DiaryEntry.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DiaryApp.Models
+{
+    public class DiaryEntry
+    {
+        [Key]
+        public int EntryId { get; set; }
+        [Required]
+        public string Title { get; set; } = string.Empty;
+        [Required]
+        public string Content { get; set; } = string.Empty;
+        [Required]
+        public DateTime Created { get; set; }
+    }
+}
