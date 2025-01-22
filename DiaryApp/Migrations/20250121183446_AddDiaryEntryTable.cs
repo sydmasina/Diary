ce1ac3869c971f081b31f6377adad530bb01a3a1
sydmasina <masinasydwell@gmail.com>
2025-01-22 11:22:18 +0200

    add database context setups and db migrations

---
+++ DiaryApp/Migrations/20250121183446_AddDiaryEntryTable.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DiaryApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDiaryEntryTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DiaryEntries",
+                columns: table => new
+                {
+                    EntryId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Content = table.Column<string>(type: "nvarchar(1500)", maxLength: 1500, nullable: false),
+                    Created = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DiaryEntries", x => x.EntryId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DiaryEntries");
+        }
+    }
+}
