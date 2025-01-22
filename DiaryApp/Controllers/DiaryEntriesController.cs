a48bfda4f2259dcf54e58390ac49572ff5e6d144
sydmasina <masinasydwell@gmail.com>
2025-01-22 13:28:35 +0200

    Update controller to pull from data from DB

--- DiaryApp/Controllers/DiaryEntriesController.cs
+++ DiaryApp/Controllers/DiaryEntriesController.cs
@@ -1,12 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DiaryApp.Data;
+using DiaryApp.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DiaryApp.Controllers
 {
     public class DiaryEntriesController : Controller
     {
+        private readonly ApplicationDbContext _dbContext;
+
+        public DiaryEntriesController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
         public IActionResult Index()
         {
-            return View();
+            List<DiaryEntry> diaryEntryList = _dbContext.DiaryEntries.ToList();
+
+            return View(diaryEntryList);
         }
     }
 }
