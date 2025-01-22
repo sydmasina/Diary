7bd5fa758abbe5d2f05acb5a63695cd5e923fbc2
sydmasina <masinasydwell@gmail.com>
2025-01-22 11:22:37 +0200

    Update model validations

--- DiaryApp/Models/DiaryEntry.cs
+++ DiaryApp/Models/DiaryEntry.cs
@@ -7,10 +7,12 @@ namespace DiaryApp.Models
         [Key]
         public int EntryId { get; set; }
         [Required]
+        [MaxLength(100)]
         public string Title { get; set; } = string.Empty;
         [Required]
+        [MaxLength(1500)]
         public string Content { get; set; } = string.Empty;
         [Required]
-        public DateTime Created { get; set; }
+        public DateTime Created { get; set; } = DateTime.Now;
     }
 }
