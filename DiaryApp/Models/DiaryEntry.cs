1b726b2bf84dd6a226d1475ce874630eb2a552ff
sydmasina <masinasydwell@gmail.com>
2025-01-23 08:44:47 +0200

    Update error handling

--- DiaryApp/Models/DiaryEntry.cs
+++ DiaryApp/Models/DiaryEntry.cs
@@ -6,13 +6,15 @@ namespace DiaryApp.Models
     {
         [Key]
         public int EntryId { get; set; }
-        [Required]
-        [MaxLength(100)]
+        [Required(ErrorMessage = "Please provide a title for your diary entry.")]
+        [MinLength(3, ErrorMessage = "Your title must be at least 3 characters long.")]
+        [MaxLength(50, ErrorMessage = "Your title can be up to 50 characters long. Keep it short and sweet!")]
         public string Title { get; set; } = string.Empty;
-        [Required]
-        [MaxLength(1500)]
+        [Required(ErrorMessage = "Don't forget to write some content for your entry!")]
+        [MinLength(10, ErrorMessage = "Your entry should be at least 10 characters. Tell us more!")]
+        [MaxLength(1000, ErrorMessage = "Your content can be up to 1000 characters long. Share your thoughts, but keep it concise!")]
         public string Content { get; set; } = string.Empty;
-        [Required]
+        [Required(ErrorMessage = "Please select a date for your diary entry.")]
         public DateTime Created { get; set; } = DateTime.Now;
     }
 }
