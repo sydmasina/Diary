c85d4a824113ef39c4415869107d9b8be5d150b6
sydmasina <masinasydwell@gmail.com>
2025-01-23 11:44:35 +0200

    Add DiaryEntryy constants

---
+++ DiaryApp/Constants/DiaryEntryConstants.cs
@@ -0,0 +1,10 @@
+﻿namespace DiaryApp.Constants
+{
+    public static class DiaryEntryConstants
+    {
+        public const int TitleMinLength = 3;
+        public const int TitleMaxLength = 100;
+        public const int ContentMinLength = 10;
+        public const int ContentMaxLength = 1000;
+    }
+}
