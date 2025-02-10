3520828cffef20574e06ac79fc3315b0b435daa7
sydmasina <masinasydwell@gmail.com>
2025-02-10 19:37:07 +0200

    Add privacy disclaimer

--- DiaryApp/Controllers/DiaryEntriesController.cs
+++ DiaryApp/Controllers/DiaryEntriesController.cs
@@ -2,6 +2,7 @@
 using DiaryApp.Data;
 using DiaryApp.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
 
 namespace DiaryApp.Controllers
 {
@@ -113,6 +114,18 @@ namespace DiaryApp.Controllers
             }
 
             return View(obj);
+        }
+
+        [HttpGet]
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
